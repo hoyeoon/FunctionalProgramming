9ce01ad1b622269830eaf0f32f77e9bbab55988a
hoyeoon <hoyeon.backend@gmail.com>
2022-01-07 16:56:19 +0900

    Part 4 Functional Interface

--- chapter4/Chapter4Section4.java
+++ chapter4/Chapter4Section4.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.function.UnaryOperator;
 
 public class Chapter4Section4 {
 	public static void main(String[] args) {
