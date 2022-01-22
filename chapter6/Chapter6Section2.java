de2c136e6d5e98c7889d342cf8594d052b0eaf54
hoyeoon <hoyeon.backend@gmail.com>
2022-01-22 12:32:43 +0900

    Part 6 Stream

---
+++ chapter6/Chapter6Section2.java
@@ -0,0 +1,19 @@
+package com.fastcampus.functionalprogramming.chapter6;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Chapter6Section2 {
+	public static void main(String[] args) {
+		Stream<Integer> numberStream = Stream.of(3, -5, 7, 10, -3);
+		Stream<Integer> filteredNumberStream = numberStream.filter(x -> x > 0);
+		List<Integer> filteredNumbers = filteredNumberStream.collect(Collectors.toList());
+		System.out.println(filteredNumbers);
+
+		List<Integer> newFilteredNumbers = Stream.of(3, -5, 7, 10, -3)
+				.filter(x -> x > 0)
+				.collect(Collectors.toList());
+		System.out.println(newFilteredNumbers);
+	}
+}
